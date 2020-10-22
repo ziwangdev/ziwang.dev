c5ddda8d1b076f1466a7342bd45c5a08f0aee1c0
Zi Wang <wang821@usc.edu>
2020-10-22 13:33:15 -0700

    starting a project to build my portfolio website for full stack development

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
+import {Button, Alert, Breadcrumb} from 'react-bootstrap'
+import {Layout, Header, Drawer, Navigation, Content} from 'react-mdl'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const REACT_VERSION = React.version;
+class App extends Component{
+
+    render(){
+        return(
+            <div>
+                {/* Uses a transparent header that draws on top of the layout's background */}
+                <div style={{height: '300px', position: 'relative'}}>
+                    <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
+                        <Header transparent title="Title" style={{color: 'white'}}>
+                            <Navigation>
+                                <a href="#">Link</a>
+                                <a href="#">Link</a>
+                                <a href="#">Link</a>
+                                <a href="#">Link</a>
+                            </Navigation>
+                        </Header>
+                        <Drawer title="Title">
+                            <Navigation>
+                                <a href="#">Link</a>
+                                <a href="#">Link</a>
+                                <a href="#">Link</a>
+                                <a href="#">Link</a>
+                            </Navigation>
+                        </Drawer>
+                        <Content />
+                    </Layout>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
